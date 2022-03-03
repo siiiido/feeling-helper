ac925d2e25d44618476473582cba8bf6733ab7c1
siiiido <discegaudere24.7@gmail.com>
2022-03-03 23:50:08 +0900

    Feat: add dog api

--- src/views/HomeMain.vue
+++ src/views/HomeMain.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <img :src="this.catUrl" alt="cat images" />
+    <img :src="this.dogUrl" alt="dog images" />
   </main>
 </template>
 
@@ -17,10 +18,16 @@ export default {
         size: "full",
         format: "json",
       },
+      dogUrl: [],
+      dogParams: {
+        limit: 1,
+        size: "full",
+        format: "json",
+      },
     };
   },
   methods: {
-    async axiosData() {
+    async axiosCatData() {
       try {
         const CAT_API_KEY = process.env.VUE_APP_CAT_KEY;
         axios.defaults.headers.common["x-api-key"] = CAT_API_KEY;
@@ -32,15 +39,32 @@ export default {
 
         return res.data[0].url;
       } catch {
-        console.log("Axios error");
+        console.log("axiosCatData error");
+      }
+    },
+    async axiosDogData() {
+      try {
+        const DOG_API_KEY = process.env.VUE_APP_DOG_KEY;
+        axios.defaults.headers.common["x-api-key"] = DOG_API_KEY;
+
+        let res = await axios.get(
+          "https://api.thedogapi.com/v1/images/search",
+          { params: { limit: this.dogParams.limit, size: this.dogParams.size } }
+        );
+
+        return res.data[0].url;
+      } catch {
+        console.log("axiosDogData error");
       }
     },
   },
 
   async created() {
-    const catData = await this.axiosData();
+    const catData = await this.axiosCatData();
+    const dogData = await this.axiosDogData();
 
     this.catUrl = catData;
+    this.dogUrl = dogData;
   },
 };
 </script>
