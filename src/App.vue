def6c1cb839e8f1204a4c09d2eb12661bd22af4b
siiiido <discegaudere24.7@gmail.com>
2022-03-04 21:25:36 +0900

    Feat: add Header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
+  <HomeHeader />
   <div class="container">
     <router-view />
   </div>
 </template>
 
 <script>
+import HomeHeader from "./components/HomeHeader.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    HomeHeader,
+  },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
 </style>
---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="home-header">HomeHeader</div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader",
+};
+</script>
+
+<style>
+.home-header{
+  height: 40px;
+  background-color: aquamarine;
+}
+</style>
